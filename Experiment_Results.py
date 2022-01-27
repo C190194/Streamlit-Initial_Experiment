f48cf09f8ce75d3fffa504647bf0bda49f17ca91
C190194 <73808832+C190194@users.noreply.github.com>
2022-01-28 02:01:22 +0800

    Add files via upload

---
+++ Experiment_Results.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Experiment Results using Baseline Methods \
+        and Team JKU's Methods") 
+
+def color_overall_row(s):
+        if s["Machine Type"] == "Overall":
+                color = "#b1e5f2"
+        else:
+                color = "transparent"
+        return [f'background-color: {color}']*len(s)
+        
+        
+
+st.subheader("Results for Full Development Dataset \
+        Using Autoencoder") 
+
+ae_df = pd.read_csv ('Baseline_AE_Results.csv')
+st.table(ae_df.style.apply(color_overall_row, axis=1))
+
+st.subheader("Results for Full Development Dataset \
+        MobileNetV2")
+
+mn_df = pd.read_csv ('Baseline_MobileNetV2_Results.csv')
+st.table(mn_df.style.apply(color_overall_row, axis=1))
+
+st.subheader("Results for Fan's Development Dataset \
+        Using Auxiliary Classification and Density Estimation")
+
+jku_df = pd.read_csv ('Primus_Fan_Results.csv')
+st.table(jku_df)
+st.write("MADE - Masked Autoencoder for Distribution Estimation")
+st.write("MAF - Masked Autoregressive Flows")
+st.write("AC - Auxiliary Classification")
+st.write("FT - Fine Tuninng") 
