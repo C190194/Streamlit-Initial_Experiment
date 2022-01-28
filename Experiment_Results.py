58e59d30bcbccedbf4d4733d2b20b06a233cac2b
C190194 <73808832+C190194@users.noreply.github.com>
2022-01-28 09:05:08 +0800

    Add files via upload

--- Experiment_Results.py
+++ Experiment_Results.py
@@ -18,20 +18,32 @@ st.subheader("Results for Full Development Dataset \
         Using Autoencoder") 
 
 ae_df = pd.read_csv ('Baseline_AE_Results.csv')
-st.table(ae_df.style.apply(color_overall_row, axis=1))
+st.table(ae_df.style.format({"Source Hmean": "{:.2f}",
+                             "Target Hmean": "{:.2f}",
+                             "AUC Hmean": "{:.2f}",
+                             "pAUC Hmean": "{:.2f}"}
+                        ).apply(color_overall_row, axis=1))
 
 st.subheader("Results for Full Development Dataset \
         MobileNetV2")
 
 mn_df = pd.read_csv ('Baseline_MobileNetV2_Results.csv')
-st.table(mn_df.style.apply(color_overall_row, axis=1))
+st.table(mn_df.style.format({"Source Hmean": "{:.2f}",
+                             "Target Hmean": "{:.2f}",
+                             "AUC Hmean": "{:.2f}",
+                             "pAUC Hmean": "{:.2f}"}
+                        ).apply(color_overall_row, axis=1))
 
 st.subheader("Results for Fan's Development Dataset \
         Using Auxiliary Classification and Density Estimation")
 
 jku_df = pd.read_csv ('Primus_Fan_Results.csv')
-st.table(jku_df)
+st.table(jku_df.style.format({"Source Hmean": "{:.1f}",
+                             "Target Hmean": "{:.1f}",
+                             "JKU Source Hmean": "{:.1f}",
+                             "JKU Target Hmean": "{:.1f}"}
+                        ))
 st.write("MADE - Masked Autoencoder for Distribution Estimation")
 st.write("MAF - Masked Autoregressive Flows")
 st.write("AC - Auxiliary Classification")
-st.write("FT - Fine Tuninng") 
+st.write("FT - Fine Tuninng")  
